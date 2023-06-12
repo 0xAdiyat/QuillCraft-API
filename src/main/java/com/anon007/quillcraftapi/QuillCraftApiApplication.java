d961adef26faf3039854854fb88a10bb89b41a82
Huss4in007 <smdhussainmolla@gmail.com>
2023-06-12 18:08:03 +0600

    QuillCraft - API : A Blog App API Backend using Spring Boot

---
+++ src/main/java/com/anon007/quillcraftapi/QuillCraftApiApplication.java
@@ -0,0 +1,13 @@
+package com.anon007.quillcraftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuillCraftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuillCraftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anon007/quillcraftapi/QuillCraftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anon007.quillcraftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuillCraftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
