4656c8cf272a13a145abc8ba4e321d558d1790c1
Huss4in007 <smdhussainmolla@gmail.com>
2023-06-15 14:21:36 +0600

    Fixed Type Error on calling delete

--- src/main/java/com/anon007/quillcraftapi/payloads/APIResponse.java
+++ src/main/java/com/anon007/quillcraftapi/payloads/APIResponse.java
@@ -4,18 +4,10 @@ import java.util.Map;
 
 public class APIResponse {
 
-    private final String message;
-
-    private final boolean isSuccess;
-    private final int status;
     private final Map<String, Object> responseMap;
 
 
     public APIResponse(String message, boolean isSuccess, int status) {
-
-        this.message = message;
-        this.isSuccess = isSuccess;
-        this.status = status;
 
         this.responseMap = Map.of(
                 "message", message,
