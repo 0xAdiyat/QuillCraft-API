1237d47f36041157194120ccc42ffd7f4286c394
Huss4in007 <smdhussainmolla@gmail.com>
2023-06-21 19:31:47 +0600

    Fix ⚒️Bug: ids for this class must be manually assigned before calling save()

--- src/main/java/com/anon007/quillcraftapi/entities/UserEntity.java
+++ src/main/java/com/anon007/quillcraftapi/entities/UserEntity.java
@@ -1,9 +1,6 @@
 package com.anon007.quillcraftapi.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
@@ -12,6 +9,7 @@ import java.util.UUID;
 public class UserEntity {
     @Id
     @Column(name = "id", columnDefinition = "BINARY(16)")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     @Column(name = "user_name", nullable = false, length = 50)
     private String name;
