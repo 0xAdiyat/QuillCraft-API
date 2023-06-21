2f45ac21464063c61bf35f88416c34f090913799
Huss4in007 <smdhussainmolla@gmail.com>
2023-06-21 21:13:56 +0600

    Add ➕Validation

--- src/main/java/com/anon007/quillcraftapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/anon007/quillcraftapi/exceptions/GlobalExceptionHandler.java
@@ -2,15 +2,38 @@ package com.anon007.quillcraftapi.exceptions;
 
 import com.anon007.quillcraftapi.payloads.APIResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errRes = new HashMap<>();
+        /*METHOD ONE
+         ex.getBindingResult().getAllErrors().forEach(e -> {
+         String fieldName = ((FieldError) e).getField();
+         String defaultMessage = e.getDefaultMessage();
+         errRes.put(fieldName, defaultMessage);
+         });*/
+        ex.getBindingResult().getFieldErrors().forEach(e -> errRes.put(e.getField(), e.getDefaultMessage()));
+
+        return new ResponseEntity<>(errRes, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public APIResponse resourceNotFoundExceptionHandler(ResourceNotFoundException resourceNotFoundException) {
-        String message = resourceNotFoundException.getMessage();
-        return new APIResponse(message, false, HttpStatus.INTERNAL_SERVER_ERROR.value());
+    public ResponseEntity<APIResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+        APIResponse response = new APIResponse();
+        response.setMessage(ex.getMessage());
+        response.setSuccess(false);
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/anon007/quillcraftapi/payloads/UserDTO.java
+++ src/main/java/com/anon007/quillcraftapi/payloads/UserDTO.java
@@ -1,19 +1,30 @@
 package com.anon007.quillcraftapi.payloads;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public class UserDTO {
     private UUID id;
-    @NotNull
+    @NotEmpty
+    @Size(min = 4, message = "Username must be minimum 4 characters long")
     private String name;
-    @Email
+    @Email(message = "Email address is not valid")
     private String email;
-    @NotNull
+    @NotEmpty(message = "Password can't be empty")
+    @Size(min = 6, max = 10, message = "Password must be min of 3 chars and max of 15 chars long")
+//    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,32}$", message = "Password must meet the following criteria: at least one digit, one lowercase letter, one uppercase letter, and one special character")
+    @Pattern.List({
+            @Pattern(regexp = "(?=.*[0-9]).+", message = "Password must contain at least one digit"),
+            @Pattern(regexp = "(?=.*[a-z]).+", message = "Password must contain at least one lowercase letter"),
+            @Pattern(regexp = "(?=.*[A-Z]).+", message = "Password must contain at least one uppercase letter"),
+            @Pattern(regexp = "(?=.*[^A-Za-z0-9]).+", message = "Password must contain at least one special character")
+    })
     private String password;
-    @NotNull
+    @NotEmpty
     private String bio;
 
     public UUID getId() {
