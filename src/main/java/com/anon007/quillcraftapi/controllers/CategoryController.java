1be846411fcfd646dacb3d32cace3f2597ca3413
Huss4in007 <smdhussainmolla@gmail.com>
2023-07-11 23:53:13 +0600

    Implement Category API

--- src/main/java/com/anon007/quillcraftapi/controllers/CategoryController.java
+++ src/main/java/com/anon007/quillcraftapi/controllers/CategoryController.java
@@ -6,10 +6,10 @@ import com.anon007.quillcraftapi.payloads.CategoryDTO;
 import com.anon007.quillcraftapi.services.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -25,4 +25,31 @@ public class CategoryController {
         CategoryDTO category = this.categoryService.createCategory(categoryDTO);
         return new APIResponse("Category created", true, HttpStatus.CREATED.value());
     }
+
+    @PostMapping("/{categoryId}")
+    public APIResponse updateCategory(@Valid @RequestBody CategoryDTO categoryDTO, @PathVariable Integer categoryId) {
+        CategoryDTO updatedCategory = this.categoryService.updateCategory(categoryDTO, categoryId);
+        return new APIResponse("Category updated for " + updatedCategory, true, HttpStatus.OK.value());
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public APIResponse deleteCategory(@PathVariable Integer categoryId) {
+        this.categoryService.deleteCategory(categoryId);
+        return new APIResponse("Deleted category: " + categoryId, true, HttpStatus.OK.value());
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> getCategory(@PathVariable Integer categoryId) {
+        CategoryDTO getCategory = this.categoryService.getCategoryById(categoryId);
+
+        return ResponseEntity.ok(getCategory);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<CategoryDTO>> getAllCategory() {
+        List<CategoryDTO> allCategories = this.categoryService.getAllCategories();
+
+        return ResponseEntity.ok(allCategories);
+    }
+
 }
