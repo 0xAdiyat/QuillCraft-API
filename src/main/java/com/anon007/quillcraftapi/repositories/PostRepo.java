63091fc48613e66de00b27015710d9f94c890066
Huss4in007 <smdhussainmolla@gmail.com>
2023-07-17 22:39:54 +0600

    PostRepo enhanced

--- src/main/java/com/anon007/quillcraftapi/repositories/PostRepo.java
+++ src/main/java/com/anon007/quillcraftapi/repositories/PostRepo.java
@@ -1,7 +1,14 @@
 package com.anon007.quillcraftapi.repositories;
 
+import com.anon007.quillcraftapi.entities.CategoryEntity;
 import com.anon007.quillcraftapi.entities.PostEntity;
+import com.anon007.quillcraftapi.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepo extends JpaRepository<PostEntity, Integer> {
+    List<PostEntity> findByUser(UserEntity user);
+
+    List<PostEntity> findByCategory(CategoryEntity user);
 }
